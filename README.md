2c1f6b7311993373f5c3a2dc299fb541d32a3e26
Abdiel Pinzon <a.pinzonc@hotmail.es>
2020-07-30 01:35:24 -0500

    Versión con reconexion funcional, estable para pruebas (incluye archivos ofuscados)

---
+++ README.md
@@ -0,0 +1,1 @@
+#### PORTAL DE MONITOREO SNMP V1 #######
---
+++ javascript/sensores.js
@@ -0,0 +1,117 @@
+(function(){
+    var App = {
+        config:{
+            api:{
+                // baseUrl: "http://172.18.227.128:3000",
+                baseUrl: "http://localhost:3000",
+                readFile: function(archivo){
+                    return `${App.config.api.baseUrl}/read/${archivo}`;
+                },
+                addSensor: function(){
+                    return `${App.config.api.baseUrl}/agregar/sensor`;
+                }
+            }
+        },
+        htmlElements:{
+            formAddSensor: document.querySelector('#form-add-sensor'),
+            tablaSensores: document.querySelector("#sensores")
+        },
+        init: function(){
+            App.readSensoresFile(App.getParams());  
+            App.htmlElements.formAddSensor.addEventListener("submit",function(e){
+                e.preventDefault();
+                App.agregarSensor(App.htmlElements.formAddSensor.sensor.value,
+                    App.htmlElements.formAddSensor.ubicacion.value)
+            })
+        },
+        events:{
+        },
+        getParams:function(){
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const file = urlParams.get('file')
+            return file;
+        },
+        readSensoresFile:function(file){
+            async function readData(){
+                const response = await App.utils.makeRequest({
+                    url: App.config.api.readFile(file)
+                })
+                // console.log(response.obj.sensores)
+                App.dibujarTabla(response.obj.sensores)
+            }
+            readData();
+        },
+        agregarSensor: function(ipsensor,ubicacion){
+            var form = {ip: ipsensor,planta:ubicacion,archivo:App.getParams()}
+            async function sendIpSensor(){
+                const response = await App.utils.postSensor({
+                    url: App.config.api.addSensor(),
+                    body:form
+                })
+                console.log(response)
+                // App.dibujarTabla(response.obj.sensores)
+                // App.readSensoresFile(App.getParams(queryString)); 
+            }
+            sendIpSensor();
+        },
+        deleteSensor:function(){
+            async function sendIP(){
+                const response = await App.utils.postSensor({
+                    // url: App.config.addSensor()
+                })
+                console.log(response)
+            }
+            sendIP();
+            // console.log(`Iniciando función de borrado para ${ip}`)
+        },
+        getSensorIp:function(ip){
+            var confirmar = confirm(`Confirmar que desea eliminar el sensor ${ip}`)
+            if(!confirmar){
+                return false;
+            }else{
+                App.deleteSensor(ip);
+            }
+        },
+        utils:{
+            makeRequest: async function({method = "get", url, body = null}){
+                const response = await fetch(url, {
+                    method,
+                    body: body ? JSON.stringify(body) : null
+                });
+                return response.json();
+            },
+            postSensor: async function({method = "post",url, body}){
+                const response = await fetch(url, {
+                    method,
+                    body: JSON.stringify(body),
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                })
+                return response.json();
+            }
+        },
+        dibujarTabla: function(ipsensores){
+            let domTabla = "";
+            var arrayIps = [];
+            ipsensores.forEach(sensor => {
+                domTabla += `<tr>
+                                <td><a href="http://${sensor.ip}/">${sensor.ip}</a></td>
+                                <td>${sensor.planta}</td>
+                                <td><button id="btn-eliminar-${sensor.ip}">Eliminar x</button></td>
+                            </tr>`
+                App.htmlElements.tablaSensores.innerHTML = domTabla;
+                arrayIps.push(sensor.ip)
+            });
+            this.addEventBorrar(arrayIps);
+        },
+        addEventBorrar: function(array){
+            array.forEach(ip => {
+                let btnEliminar = document.getElementById(`btn-eliminar-${ip}`);
+                btnEliminar.addEventListener("click",function(){App.getSensorIp(ip)});
+            });
+        }
+    }
+    App.init();
+})();
