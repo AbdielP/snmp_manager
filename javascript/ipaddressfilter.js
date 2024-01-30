149c93ccb74925b630e031c5776e00a58d6d7642
abdielp <APP701037@cwpanama.com>
2024-01-30 03:05:18 -0500

    corrección de error al aplicar clase error

--- javascript/ipaddressfilter.js
+++ javascript/ipaddressfilter.js
@@ -4,10 +4,16 @@ input.addEventListener('blur', () => {
 })
 
 const ValidateIPaddress = (ipaddress) => {
- if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))
-    return (true)
-    input.className =  `${ input.className } error`
-    alert('Verificar dirección IP. Únicamente en formato ipV4 0.0.0.0')
-    return (false)
+    input.classList.remove('error')
+    if(ipaddress.length > 0) {
+
+        if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))
+        {
+            return (true)
+        }
+        input.className =  `${ input.className } error`
+        alert('Verificar dirección IP. Únicamente en formato ipV4 0.0.0.0')
+        return (false)
+    }
 }
 
